1ebd338e41724f0371cce70d157c64eeae4375a8
Johnny Gérard <john@jgerard.dev>
2023-07-15 17:36:29 +0200

    feat: navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// This dummy component is a temporary placeholder.
+@Component({
+  template: '<p>Dummy works!</p>'
+})
+export class DummyComponent { }
+
+export const routes: Routes = [
+  { path: '', component: DummyComponent },
+  { path: 'roadmap', component: DummyComponent },
+  { path: 'add-feedback', component: DummyComponent },
+  { path: 'feedback/:id', component: DummyComponent },
+  { path: 'edit-feedback/:id', component: DummyComponent },
+  { path: '**', redirectTo: '' }
+];
