3bd4a8969523393a9fa1eadb13f7415f4b6e60f8
Johnny Gérard <john@jgerard.dev>
2023-07-19 21:22:23 +0200

    feat: add-feedback-link component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProductRequestCategory } from './Types/product-request-category.enum';
-import { SuggestionComponent } from './suggestion/suggestion.component';
+import { AddFeedbackLinkComponent } from './add-feedback-link.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SuggestionComponent],
+  imports: [CommonModule, RouterOutlet, AddFeedbackLinkComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
