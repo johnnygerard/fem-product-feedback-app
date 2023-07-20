1222b22220e4540f227a0f75cae83329161d7c46
Johnny Gérard <john@jgerard.dev>
2023-07-20 16:33:11 +0200

    feat: logo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FeedbackCategory } from './Types/feedback-category.enum';
-import { SortComponent } from './sort/sort.component';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SortComponent],
+  imports: [CommonModule, RouterOutlet, LogoComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  protected category = FeedbackCategory.FEATURE;
-
   constructor(location: Location) {
     location.go('/');
   }
