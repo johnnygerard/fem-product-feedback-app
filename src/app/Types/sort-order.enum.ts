e66ed9e0f9e58c575a0a473390ab294d82096ca5
Johnny Gérard <john@jgerard.dev>
2023-07-20 05:37:19 +0200

    feat: sort component (#10)

---
+++ src/app/Types/sort-order.enum.ts
@@ -0,0 +1,9 @@
+export enum SortOrder {
+  /**
+   * Default sort order
+   */
+  MOST_UPVOTES = 'Most Upvotes',
+  LEAST_UPVOTES = 'Least Upvotes',
+  MOST_COMMENTS = 'Most Comments',
+  LEAST_COMMENTS = 'Least Comments',
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FeedbackCategory } from './Types/feedback-category.enum';
-import { AddFeedbackLinkComponent } from './add-feedback-link.component';
+import { SortComponent } from './sort/sort.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AddFeedbackLinkComponent],
+  imports: [CommonModule, RouterOutlet, SortComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
