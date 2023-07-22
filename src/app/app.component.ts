85d3771ad63082fc00a4a2357ec6c8ce437d0e4a
Johnny Gérard <john@jgerard.dev>
2023-07-22 05:14:31 +0200

    refactor: suggestion component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { CommentCountComponent } from './comment-count/comment-count.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CommentCountComponent],
+  imports: [CommonModule, RouterOutlet, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
