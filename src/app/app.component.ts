deea7028be5558e3440734d33a2d1f64f8192b52
Johnny Gérard <john@jgerard.dev>
2023-07-24 22:51:18 +0200

    feat: feedback-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import { FeedbackPageComponent } from './feedback-page/feedback-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomePageComponent],
+  imports: [CommonModule, RouterOutlet, FeedbackPageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(location: Location) {
-    location.go('/');
+    location.go('/feedback/2');
   }
 }
