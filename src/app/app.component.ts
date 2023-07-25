f84b5810301e313e2014313ca229c42b03f1221e
Johnny Gérard <john@jgerard.dev>
2023-07-26 00:02:53 +0200

    feat: select component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SelectComponent } from './select/select.component';
-import { FeedbackCategory } from './Types/feedback-category.enum';
-import { FormsModule } from '@angular/forms';
+import { AddFeedbackComponent } from './add-feedback/add-feedback.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule, SelectComponent],
+  imports: [CommonModule, RouterOutlet, AddFeedbackComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  value = FeedbackCategory.FEATURE;
-  options = [
-    FeedbackCategory.FEATURE,
-    FeedbackCategory.UI,
-    FeedbackCategory.UX,
-    FeedbackCategory.ENHANCEMENT,
-    FeedbackCategory.BUG,
-  ]
   constructor(location: Location) {
-    // location.go('/');
+    // location.go('/add-feedback');
   }
 }
