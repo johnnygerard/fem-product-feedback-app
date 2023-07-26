8b363e66e914fd8007a9df9bdf51d91217bbf01e
Johnny Gérard <john@jgerard.dev>
2023-07-26 03:54:51 +0200

    feat: update route configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,15 @@
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { AddFeedbackComponent } from './add-feedback/add-feedback.component';
+import { EditFeedbackPageComponent } from './edit-feedback-page/edit-feedback-page.component';
 import { FeedbackPageComponent } from './feedback-page/feedback-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RoadmapPageComponent } from './roadmap/roadmap-page/roadmap-page.component';
-
-// This dummy component is a temporary placeholder.
-@Component({
-  template: '<p>Dummy works!</p>'
-})
-export class DummyComponent { }
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'roadmap', component: RoadmapPageComponent },
   { path: 'add-feedback', component: AddFeedbackComponent },
   { path: 'feedback/:id', component: FeedbackPageComponent },
-  { path: 'edit-feedback/:id', component: DummyComponent },
+  { path: 'edit-feedback/:id', component: EditFeedbackPageComponent },
   { path: '**', redirectTo: '' }
 ];
