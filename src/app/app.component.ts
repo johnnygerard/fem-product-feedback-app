bbd0aeb5174abfe70497d5ce3be99c63e3bf28f9
Johnny Gérard <john@jgerard.dev>
2023-07-26 22:45:32 +0200

    feat: remove test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule, Location } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { AddFeedbackComponent } from './add-feedback/add-feedback.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AddFeedbackComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(location: Location) {
-    // location.go('/add-feedback');
-  }
-}
+export class AppComponent { }
